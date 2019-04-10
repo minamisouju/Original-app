cfabe0bd7a1a42a3042859c3a5fc94e5f9d66bad
minamisouju <sky.sight50@gmail.com>
2019-04-10 22:17:36 +0900

    [add]

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'contents#home'
   get '/success', to:'contents#success', as:'success'
   match '/tweet', to:'contents#tweet', via: [:post], as: 'tweet'
+  match '/auto_tweet', to:'contents#auto_tweet', via: [:post], as: 'auto_tweet'
   resources :contents, except:[:show, :edit, :update]
 end
